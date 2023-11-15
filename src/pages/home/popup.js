3c92e0f3bcaea097a3c4e8de1f83f19918a11612
Luis Pardo <luispardosuarez@gmail.com>
2023-11-15 14:55:42 +0100

    chage some js code

--- src/pages/home/popup.js
+++ src/pages/home/popup.js
@@ -1,32 +1,45 @@
 import '../../assets/styles/popup-style.scss'
 
-<script>
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
+    // Añade el contenido al elemento con id 'popup'
     document.getElementById('popup').innerHTML = `
         <body>
-            <button id="startButton">Inicio</button>
+
+            <button onclick="togglePopup()">Inicio</button>
                 
-            <div class="panel" id="start" data-on="off" onclick="this.setAttribute('data-on','off')">
+            <div class="panel" id="start" data-on="off" onclick="togglePopup()">
                 <div>
                     <p>Has llegado al mejor juego de duelos del lejano Oeste, en el que podrás participar hasta con 25 jugadores.</p>
                     <p>Elige tu avatar y enfréntate de manera aleatoria en duelo con el resto de jugadores.</p>
                     <p>¡Si ganas a tu adversario, seguirás jugando hasta que sólo quede uno!</p>
-                    <button class="popup" id="startButton">Iniciar partida</button>
+
+                    <button class="popup" onclick="redirectToNextPage()">Iniciar partida</button>
                 </div>
             </div>
+
         </body>
     `;
 
-    
-    document.getElementById('startButton').addEventListener('click', function() {
-        redirectToNextPage();
-    });
-
+    // Función para redirigir a la siguiente página
     function redirectToNextPage() {
         window.location.href = 'players.html';
     }
 
+    // Función para mostrar/ocultar el panel
+    function togglePopup() {
+        var startPanel = document.getElementById('start');
+        var dataOn = startPanel.getAttribute('data-on');
+        
+        if (dataOn === 'off') {
+            startPanel.setAttribute('data-on', 'on');
+        } else {
+            startPanel.setAttribute('data-on', 'off');
+        }
+    }
+
 });
-</script>
+
 
