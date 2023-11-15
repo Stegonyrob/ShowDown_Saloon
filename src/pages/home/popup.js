3f653af7e0070732aaa4854da386805194fd0e4b
Luis Pardo <luispardosuarez@gmail.com>
2023-11-15 14:48:07 +0100

    test popup failed

--- src/pages/home/popup.js
+++ src/pages/home/popup.js
@@ -1,28 +1,32 @@
 import '../../assets/styles/popup-style.scss'
 
-document.getElementById('popup').innerHTML = `
+<script>
+document.addEventListener('DOMContentLoaded', function() {
 
-<body>
+    document.getElementById('popup').innerHTML = `
+        <body>
+            <button id="startButton">Inicio</button>
+                
+            <div class="panel" id="start" data-on="off" onclick="this.setAttribute('data-on','off')">
+                <div>
+                    <p>Has llegado al mejor juego de duelos del lejano Oeste, en el que podrás participar hasta con 25 jugadores.</p>
+                    <p>Elige tu avatar y enfréntate de manera aleatoria en duelo con el resto de jugadores.</p>
+                    <p>¡Si ganas a tu adversario, seguirás jugando hasta que sólo quede uno!</p>
+                    <button class="popup" id="startButton">Iniciar partida</button>
+                </div>
+            </div>
+        </body>
+    `;
 
-    <button onclick="document.getElementById('start').setAttribute('data-on','on')">Inicio</button>
-        
-    <div class="panel" id="start" data-on="off" onclick="this.setAttribute('data-on','off')">
-        <div>
-            <p>Has llegado al mejor juego de duelos del lejano Oeste, en el que podrás participar hasta con 25 jugadores.
-            </p>
-            <p>Elige tu avatar y enfréntate de manera aleatoria en duelo con el resto de jugadores.</p>
-            <p>¡Si ganas a tu adversario, seguirás jugando hasta que sólo quede uno!</p>
+    
+    document.getElementById('startButton').addEventListener('click', function() {
+        redirectToNextPage();
+    });
 
-            <button class="popup" onclick="redirectToNextPage()">Iniciar partida</button>
-        </div>
-    </div>
+    function redirectToNextPage() {
+        window.location.href = 'players.html';
+    }
 
-</body>
-`;
-
-
-function redirectToNextPage() {
-  window.location.href = 'players.html';
-  }
-
+});
+</script>
 
