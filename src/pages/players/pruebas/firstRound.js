a2f70bda54ff191e01646bad70db91dcc34e5552
Stella Maris González Robert <stegonyrob@yahoo.es>
2023-11-16 12:38:06 +0100

    eliminacion de codigo inecesario

--- src/pages/players/pruebas/firstRound.js
+++
@@ -1,5 +0,0 @@
-export function selectFirstRoundPlayers(players) {
- const player1 = players[Math.floor(Math.random() * players.length)];
- const player2 = players[Math.floor(Math.random() * players.length)];
- return [player1 , player2];
-}
--- src/pages/players/pruebas/subsequentRounds.js
+++
@@ -1,8 +0,0 @@
-export function handleSubsequentRounds(players, selectFirstRoundPlayers, deletePlayer) {
-    while (players.length > 1) {
-    const [player1 , player2] = selectFirstRoundPlayers(players);
-    const winner = initDuels(player1 , player2);
-    players = deletePlayer(players, winner.name);
-    }
-   }
-   
